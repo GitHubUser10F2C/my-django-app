97de6341a27dccd8d979e4edb8a262273f5e7e1c
GitHubUser10F2C <nelson.soto@alumnos.ucn.cl>
2022-05-03 22:35:24 -0400

    feat: add person and pet views

--- personalProject/personalProject/urls.py
+++ personalProject/personalProject/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+from petclub.views import PersonAPIView, PetAPIView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('pets/', PetAPIView.as_view(), name="pets"),
+    path('persons/', PersonAPIView.as_view(), name="persons"),
 ]
--- personalProject/petclub/views.py
+++ personalProject/petclub/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class PersonAPIView(APIView):
+    def get(self, request):
+        return Response(data="estoy en el get del Person", status=200)
+
+    def patch(self, request):
+        return Response(data="Estoy en el patch del Person", status=200)
+
+    def delete(self, request):
+        return Response(data="Estoy en el delete del Person", status=200)
+        
+    def post(self, request):
+        return Response(data="Estoy en el post del Person", status=200)
+
+
+class PetAPIView(APIView):
+    def get(self, request):
+        return Response(data="estoy en el get del Pet", status=200)
+
+    def patch(self, request):
+        return Response(data="Estoy en el patch del Pet", status=200)
+
+    def delete(self, request):
+        return Response(data="Estoy en el delete del Pet", status=200)
+
+    def post(self, request):
+        return Response(data="Estoy en el post del Pet", status=200)
